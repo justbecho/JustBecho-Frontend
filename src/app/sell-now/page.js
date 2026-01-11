f98e89977173b6b830117776fd86039c2a4fb42c
karan-negi01 <btwitskaranhere@gmail.com>
2026-01-11 18:44:31 +0530

    karl brand name12

--- src/app/sell-now/page.js
+++ src/app/sell-now/page.js
@@ -170,7 +170,7 @@ export default function SellNowPage() {
         'alexander mcqueen': 'Alexander McQueen',
         'yves saint laurent': 'Yves Saint Laurent',
         'ysl': 'YSL',
-        'Karl Lagerfeld': 'Karl Lagerfeld',
+        'Kl': 'Karl Lagerfeld',
         'tom ford': 'Tom Ford',
         'calvin klein': 'Calvin Klein',
         'ck': 'Calvin Klein',
